385acde57e0d7e3eef5bedfa941615887458aa71
Sharan Pai <115891269+SharanPai21@users.noreply.github.com>
2025-06-14 19:07:22 +0530

    Add files via upload

---
+++ prajectsales.sql
@@ -0,0 +1,12 @@
+create database projectsales
+
+use projectsales
+
+select * from sales
+
+select * from INFORMATION_SCHEMA.COLUMNS 
+where TABLE_NAME = 'sales'
+
+alter table sales 
+alter column total_sale float
+
